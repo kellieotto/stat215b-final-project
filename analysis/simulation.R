26eb0ec55e33fecb3ba1dad6db2b4a4c6ee4830a
kellieotto <kellieotto@berkeley.edu>
2015-04-13 22:36:20 -0700

    Add simulation

---
+++ analysis/simulation.R
@@ -0,0 +1,122 @@
+#################### Simulation
+
+### Simple design from Freedman (Weighting Regressions by P Scores)
+### We assume a treatment effect b which depends on W1: b = 1 if W1 > 0.75, b=-1 if W1 < 0.75
+### Y = a + bX + c1W1 + c2W2 + dU is the response model
+### X = I(e1 + f1W1 + f2W2 + V > 0) is the selection model for treatment when not randomized
+### S = I(e2 + g1W1 + g2W2 + g3W3 + R > 0) is the model for selection into the RCT
+### C = I(e3 + h2W2 + h3W3 + Q > 0) is the model for compliance
+### U, V, R, T are N(0,1); U, V, R, T, (W1, W2, W3) are mutually independent
+
+
+rm(list=ls())
+library(Matching)
+library(MASS)
+#library(devtools)
+#install_github("kellieotto/ModelMatch/ModelMatch")
+#library(ModelMatch)
+library(randomForest)
+library(rpart)
+
+## Setup
+sims <- 10
+
+# (U, V, R, Q, W1, W2, W3) are multivariate normal
+a <- c1 <- d <- 1
+c2 <- 2
+e1 <- e2 <- e3 <- 0.5
+f1 <- g2 <- 0.25
+f2 <- g3 <- 0.75
+g1 <- h2 <- h3 <- 0.5
+Sigma <- diag(rep(1,7))
+Sigma[5,5] <- 2
+Sigma[6,6] <- 1
+Sigma[7,7] <- 3
+Sigma[5,6] <- Sigma[6,5] <- 1
+Sigma[5,7] <- Sigma[7,5] <- Sigma[7,6] <- Sigma[6,7] <- 0.5
+mu <- c(0, 0, 0, 0, 0.5, 1, -2)
+
+# set up storage
+tpatt <- true_patt <- rct_sate <- tpatt_unadj <- rep(0, sims)
+
+for(i in 1:sims){
+  # Pick target sample size
+  print(i)
+  popsize <- 30000
+  rctpopsize <- 5000
+  rctsample <- sample(1:popsize, rctpopsize)
+  observsample <- (1:popsize)[!(1:popsize %in% rctsample)]
+  
+  # Data for the whole population
+  var <- mvrnorm(popsize, mu=mu, Sigma=Sigma, empirical = F)
+  U <- var[,1]
+  V <- var[,2]
+  R <- var[,3]
+  Q <- var[,4]
+  W1 <- var[,5]
+  W2 <- var[,6]
+  W3 <- var[,7]
+  b <- ifelse(W1 > 0.75, 1, -1)
+  X = as.numeric((e1 + f1*W1 + f2*W2 + V) > 0 )
+  S = as.numeric(e2 + g1*W1 + g2*W2 + g3*W3 + R > 0) 
+  C = as.numeric(e3 + h2*W2 + h3*W3 + Q > 0) 
+  Xobs <- ifelse(C == 1, X, 0)
+  Y = a + b*Xobs + c1*W1 + c2*W2 + d*U
+  dat <- data.frame(Y, X, Xobs, S, C, W1, W2, W3)
+  
+  # Set up the RCT
+  rct <- dat[rctsample,]
+  rct <- rct[rct$S == 1,]
+  rct$X <- sample(c(0,1), size = nrow(rct), replace = TRUE)
+  rct$Xobs <- ifelse(rct$C == 1, rct$X, 0)
+  
+  # Predict who is a complier using W1, W2, W3
+  suppressWarnings(complier_mod <- randomForest(C~W1+W2+W3, data = rct)) # estimate propensity of compliance
+  rct$C_pscore <- predict(complier_mod, rct)
+  rct$Chat <- rep(1, nrow(rct))
+  rct$Chat[rct$C == 0] <- as.numeric(rct$C_pscore[rct$C == 0] >= 0.5)
+  dat$C_pscore <- predict(complier_mod, dat)
+  compliers <- rct[rct$Chat == 1,]
+  
+  
+  # Set up the non-randomized trial. 
+  nrt <- dat[observsample,]
+  nrt <- nrt[nrt$S==0,]
+  nrt$Chat <- rep(1, nrow(nrt))
+  nrt$Chat[nrt$C == 0] <- as.numeric(nrt$C_pscore[nrt$C == 0] >= 0.5)
+  nrt_compliers <- nrt[nrt$Chat == 1,]
+  
+  # Fit a regression to the compliers in the RCT, use it to predict response in population "compliers"
+  response_mod <- randomForest(Y~X + W1 + W2 + W3, data = compliers)
+  nrt_tr_counterfactual <- cbind(nrt_compliers[,c("W1", "W2", "W3")], "X" = rep(1, nrow(nrt_compliers)))
+  nrt_ctrl_counterfactual <- cbind(nrt_compliers[,c("W1", "W2", "W3")], "X" = rep(0, nrow(nrt_compliers)))
+  nrt_compliers$Yhat_1 <- predict(response_mod, nrt_tr_counterfactual)
+  nrt_compliers$Yhat_0 <- predict(response_mod, nrt_ctrl_counterfactual)
+  
+  
+  
+  # Compute the estimator
+#  term1 <- mean(nrt_compliers$C_pscore[nrt_compliers$X == 1]*nrt_compliers$Yhat[nrt_compliers$X==1])
+#  term2 <- mean(nrt_compliers$C_pscore[nrt_compliers$X == 0]*nrt_compliers$Yhat[nrt_compliers$X==0])
+  term1 <- mean(nrt_compliers$C_pscore[nrt_compliers$X == 1]*nrt_compliers$Yhat_1[nrt_compliers$X==1])
+  term2 <- mean(nrt_compliers$C_pscore[nrt_compliers$X == 1]*nrt_compliers$Yhat_0[nrt_compliers$X==1])
+  tpatt[i] <- term1 - term2
+  
+  # Compare to other estimators
+  true_patt[i] <- mean(b[X == 1])
+  # SATE
+  rct_sate[i] <- mean(rct$Y[rct$X == 1]) - mean(rct$Y[rct$X==0])
+  # Hartman et al - doesn't account for noncompliance
+  response_mod2 <- randomForest(Y~X + W1 + W2 + W3, data = rct)
+  nrt_tr_counterfactual <- cbind(nrt[,c("W1", "W2", "W3")], "X" = rep(1, nrow(nrt)))
+  nrt_ctrl_counterfactual <- cbind(nrt[,c("W1", "W2", "W3")], "X" = rep(0, nrow(nrt)))
+  nrt$Yhat_1 <- predict(response_mod2, nrt_tr_counterfactual)
+  nrt$Yhat_0 <- predict(response_mod2, nrt_ctrl_counterfactual)
+  term1 <- mean(nrt$Yhat_1[nrt$X==1])
+  term2 <- mean(nrt$Yhat_0[nrt$X==1])
+  tpatt_unadj[i] <- term1 - term2
+
+}
+
+res <- cbind(true_patt, tpatt, tpatt_unadj, rct_sate)
+(res)
