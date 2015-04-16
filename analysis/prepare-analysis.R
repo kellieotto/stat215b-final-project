83e620d08f8e345393a18339b83e13ff22efb9f3
jvpoulos <jvpoulos@gmail.com>
2015-04-16 12:59:20 -0700

    some progress made on  analysis script

--- analysis/prepare-analysis.R
+++ analysis/prepare-analysis.R
@@ -1,10 +1,33 @@
 ## Imports NHIS and OHIE datasets and creates outcome vectors and common covariates for the analysis 
 
 # Libraries
+library(weights)
 
-# Define data directory
-data.directory <- "~/Dropbox/github/stat215b-final-project/data/OHIE_Public_Use_Files/OHIE_Data"
+# Define directory for analysis 
+directory <- "~/Dropbox/github/stat215b-final-project/analysis"
 
+# Source data prep scripts
+source(file.path(directory,"prepare-ohie.R"))
+source(file.path(directory,"prepare-nhis.R"))
+
+## Create numeric vectors for treatment, # of HH members, and compliance status
+
+# Treatment assignment
+treatment <- ifelse(ohie$treatment=="Selected",1,0)
+
+# Assignment is random only conditional on # of HH members on waiting list 
+n.hh <- dummify(ohie$numhh_list)
+
+# Compliance is "ever on Medicaid" during study period
+# (variable used for analysis of hospital discharge data in Taubman et al. 2014)
+insurance <- ifelse(ohie$ohp_all_ever_firstn_30sep2009=="Enrolled",1,0)
+
+table(insurance, treatment) # there's two-way crossover?
+
+## Create vectors for health care use outcomes
+
+
+## Create vectors for common covariates
 
 # create at or below 138% fpl dummy
 x.sa$below.138.fpl = ifelse(x.sa$povrati3 <= 1380 , 1 , 0 ) 
