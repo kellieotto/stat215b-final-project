fb0704d8a9bfb54aef472ee308569ca2dfd216d7
jvpoulos <jvpoulos@gmail.com>
2015-04-20 16:08:11 -0700

    add code for response regression

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -11,20 +11,39 @@ directory <- "~/Dropbox/github/stat215b-final-project/analysis"
 source(file.path(directory,"prepare-analysis.R"))
 source(file.path(directory,"SuperLearner.R"))
 
-# Create dfs containing common features for RCT and observational study
-X.ohie <- na.omit(data.frame(n.hh,  # need to omit rows containing any NA
-                             gender, 
-                             age.20to49,
-                             age.50to64,
-                             white,
-                             black,
-                             hisp,
-                             diabetes,
-                             asthma,
-                             bp,
-                             heart,
-                             education,
-                             income)) 
+## RCT: Create dfs for common features and responses
+
+Y.ohie <- na.omit(data.frame("any.visit"=any.visit, # remove missing rows
+                             "num.visit"=num.visit,
+                             "any.out"=any.out,
+                             "num.out"=num.out)) 
+
+X.ohie <-   na.omit(data.frame(n.hh,  # remove missing rows
+                     gender, 
+                     age.20to49,
+                     age.50to64,
+                     white,
+                     black,
+                     hisp,
+                     diabetes,
+                     asthma,
+                     bp,
+                     heart,
+                     education,
+                     income))
+
+# Impute missing feature values using proximity from randomForest
+y.col <- 1:ncol(Y.ohie) # number of responses
+set.seed(42)
+X.ohie.response <- lapply(y.col, rfImpute(X.ohie[as.numeric(rownames(Y.ohie)),], # feature rows match response rows
+                                           y=Y.ohie[,y.col]))
+
+set.seed(42)
+X.ohie.insurance <- rfImpute(X.ohie, # impute missing feature values using proximity from randomForest
+                             factor(insurance)) 
+                             
+# NRT: Create dfs for common features and reponses
+
 X.nhis <-   na.omit(data.frame(n.hh.nhis, # need to omit rows containing any NA
                              gender.nhis, 
                              "age.20to49"=age.20to49.nhis,
@@ -39,16 +58,8 @@ X.nhis <-   na.omit(data.frame(n.hh.nhis, # need to omit rows containing any NA
                              education.nhis,
                              income.nhis))
 
-# Create dfs for outcomes 
-Y.ohie <- data.frame("any.visit"=any.visit[as.numeric(rownames(X.ohie))], # remove rows with missing predictors
-                    "num.visit"=num.visit[as.numeric(rownames(X.ohie))],
-                    "any.hosp"=any.hosp[as.numeric(rownames(X.ohie))], 
-                    "any.out"=any.out[as.numeric(rownames(X.ohie))],
-                    "num.out"=num.out[as.numeric(rownames(X.ohie))]) 
-
 Y.nhis <- data.frame("any.visit"=nhis.any.visit[as.numeric(rownames(X.nhis))], # remove rows with missing predictors
                      "num.visit"=nhis.num.visit[as.numeric(rownames(X.nhis))],
-                     "any.hosp"=nhis.any.hosp[as.numeric(rownames(X.nhis))], 
                      "any.out"=nhis.any.out[as.numeric(rownames(X.nhis))],
                      "num.out"=nhis.num.out[as.numeric(rownames(X.nhis))]) 
 
@@ -64,8 +75,8 @@ if(run){
                                cvControl=list(stratifyCV=TRUE))
   C.pscore <- complier.mod$SL.predict   # Store predictions
 }
-complier.mod <- suppressWarnings(randomForest(x=X.ohie,
-                                              y=insurance[as.numeric(rownames(X.ohie))])) # use rf regression for now
+complier.mod <- suppressWarnings(randomForest(X.ohie.insurance,
+                                              insurance)) # use rf regression for now
 rct.compliers <- data.frame("treatment"=treatment[as.numeric(rownames(X.ohie))],
                             "insurance"=insurance[as.numeric(rownames(X.ohie))],
                             "C.pscore"=complier.mod$predicted, # change to complier.mod$SL.predict
@@ -78,9 +89,8 @@ nrt.compliers <- data.frame("C.pscore"=predict(complier.mod, X.ohie, type = "res
                             "C.hat"=ifelse(complier.mod$predicted>=0.5,1,0))
 
 # Fit a regression to the compliers in the RCT, use it to predict response in population "compliers"
-y.col <- 1:ncol(Y.ohie) # number of responses
 response.mod <- lapply(y.col, randomForest, x=cbind(rct.compliers$treatment[rct.compliers$complier==1],
-                                                    X.ohie[rct.compliers$complier==1,]),
+                                                    X.ohie.response[rct.compliers$complier==1,]),
                                                     y=Y.ohie[as.numeric(rownames(X.ohie[rct.compliers$complier==1,])),y.col])
 
 response_mod <- randomForest(Y~Tt + W1 + W2 + W3, data = rct_compliers)
