f4692aa3725a5abc9fd756f6a78589d8d8d0e40b
kellieotto <kellieotto@berkeley.edu>
2015-04-20 22:07:02 -0700

    Predict compliance in rct tr group with random forest

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -11,39 +11,22 @@ directory <- "~/Dropbox/github/stat215b-final-project/analysis"
 source(file.path(directory,"prepare-analysis.R"))
 source(file.path(directory,"SuperLearner.R"))
 
-## RCT: Create dfs for common features and responses
-
-Y.ohie <- na.omit(data.frame("any.visit"=any.visit, # remove missing rows
-                             "num.visit"=num.visit,
-                             "any.out"=any.out,
-                             "num.out"=num.out)) 
-
-X.ohie <-   na.omit(data.frame(n.hh,  # remove missing rows
-                     gender, 
-                     age.20to49,
-                     age.50to64,
-                     white,
-                     black,
-                     hisp,
-                     diabetes,
-                     asthma,
-                     bp,
-                     heart,
-                     education,
-                     income))
-
-# Impute missing feature values using proximity from randomForest
-y.col <- 1:ncol(Y.ohie) # number of responses
-set.seed(42)
-X.ohie.response <- lapply(y.col, rfImpute(X.ohie[as.numeric(rownames(Y.ohie)),], # feature rows match response rows
-                                           y=Y.ohie[,y.col]))
-
-set.seed(42)
-X.ohie.insurance <- rfImpute(X.ohie, # impute missing feature values using proximity from randomForest
-                             factor(insurance)) 
-                             
-# NRT: Create dfs for common features and reponses
-
+# Create dfs containing common features for RCT and observational study
+X.ohie <- na.omit(data.frame(n.hh,  # need to omit rows containing any NA
+                             gender, 
+                             age.20to49,
+                             age.50to64,
+                             white,
+                             black,
+                             hisp,
+                             diabetes,
+                             asthma,
+                             bp,
+                             heart,
+                             education,
+                             income)) 
+treatment.ohie <- treatment[as.numeric(rownames(X.ohie))]
+insurance.ohie <- insurance[as.numeric(rownames(X.ohie))]
 X.nhis <-   na.omit(data.frame(n.hh.nhis, # need to omit rows containing any NA
                              gender.nhis, 
                              "age.20to49"=age.20to49.nhis,
@@ -58,12 +41,21 @@ X.nhis <-   na.omit(data.frame(n.hh.nhis, # need to omit rows containing any NA
                              education.nhis,
                              income.nhis))
 
+# Create dfs for outcomes 
+Y.ohie <- data.frame("any.visit"=any.visit[as.numeric(rownames(X.ohie))], # remove rows with missing predictors
+                    "num.visit"=num.visit[as.numeric(rownames(X.ohie))],
+                    "any.hosp"=any.hosp[as.numeric(rownames(X.ohie))], 
+                    "any.out"=any.out[as.numeric(rownames(X.ohie))],
+                    "num.out"=num.out[as.numeric(rownames(X.ohie))]) 
+
 Y.nhis <- data.frame("any.visit"=nhis.any.visit[as.numeric(rownames(X.nhis))], # remove rows with missing predictors
                      "num.visit"=nhis.num.visit[as.numeric(rownames(X.nhis))],
+                     "any.hosp"=nhis.any.hosp[as.numeric(rownames(X.nhis))], 
                      "any.out"=nhis.any.out[as.numeric(rownames(X.nhis))],
                      "num.out"=nhis.num.out[as.numeric(rownames(X.nhis))]) 
 
 # Predict who is a complier in RCT and NRT
+load("analysis.RData")
 run <- FALSE
 if(run){
   set.seed(42)
@@ -75,22 +67,25 @@ if(run){
                                cvControl=list(stratifyCV=TRUE))
   C.pscore <- complier.mod$SL.predict   # Store predictions
 }
-complier.mod <- suppressWarnings(randomForest(X.ohie.insurance,
-                                              insurance)) # use rf regression for now
-rct.compliers <- data.frame("treatment"=treatment[as.numeric(rownames(X.ohie))],
-                            "insurance"=insurance[as.numeric(rownames(X.ohie))],
-                            "C.pscore"=complier.mod$predicted, # change to complier.mod$SL.predict
-                            "C.hat"=ifelse(complier.mod$predicted>=0.5,1,0),
+
+
+complier.mod <- suppressWarnings(randomForest(x=X.ohie[treatment.ohie == 1,],                                        y=insurance.ohie[treatment.ohie==1])) # use rf regression for now
+rct.compliers <- data.frame("treatment"=treatment.ohie,
+                            "insurance"=insurance.ohie,
+                            "C.pscore"=predict(complier.mod, X.ohie), # change to complier.mod$SL.predict
+                            "C.hat"=ifelse(predict(complier.mod, X.ohie)>=0.5,1,0),
                             "complier"=0)
 rct.compliers$complier[rct.compliers$treatment==1 & rct.compliers$insurance==1] <- 1 # true compliers in the treatment group
 rct.compliers$complier[rct.compliers$treatment==0 & rct.compliers$C.hat==1] <- 1 # predicted compliers from the control group
+save(complier.mod, rct.compliers, file = "complier-mod-rf.RData")
 
-nrt.compliers <- data.frame("C.pscore"=predict(complier.mod, X.ohie, type = "response"),
-                            "C.hat"=ifelse(complier.mod$predicted>=0.5,1,0))
+nrt.compliers <- data.frame("C.pscore"=predict(complier.mod, X.nhis),
+                            "C.hat"=ifelse(predict(complier.mod, X.nhis)>=0.5,1,0))
 
 # Fit a regression to the compliers in the RCT, use it to predict response in population "compliers"
+y.col <- 1:ncol(Y.ohie) # number of responses
 response.mod <- lapply(y.col, randomForest, x=cbind(rct.compliers$treatment[rct.compliers$complier==1],
-                                                    X.ohie.response[rct.compliers$complier==1,]),
+                                                    X.ohie[rct.compliers$complier==1,]),
                                                     y=Y.ohie[as.numeric(rownames(X.ohie[rct.compliers$complier==1,])),y.col])
 
 response_mod <- randomForest(Y~Tt + W1 + W2 + W3, data = rct_compliers)
