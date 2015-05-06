b8646e6339e574ac67b649f7bcff81e801ef6f65
jvpoulos <jvpoulos@gmail.com>
2015-05-06 11:31:48 -0700

    redo Table 1

--- analysis/estimator-compare-plots.R
+++ analysis/estimator-compare-plots.R
@@ -2,8 +2,6 @@
 
 # Libraries
 library(ggplot2)
-library(reporttools)
-library(randomForest)
 
 ### Confidence intervals for the estimates
 B <- 100
@@ -172,25 +170,4 @@ het.plot.all[[1]] # any.visit
 #het.plot.all[[2]] # num.visit
 het.plot.all[[3]] # any.out
 #het.plot.all[[4]] # num.out
-
-# Create table similar to Table 1 of Hartman et al. 
-latex <- FALSE
-if() {
-  rct.nrt.tab <- rbind(cbind(study="OHIE",X.ohie.response,
-                             Y.ohie.response[c("any.visit","any.out")]),
-                       cbind(study="NHIS",nrt.tr.counterfactual,
-                             Y.nhis[c("any.visit","any.out")][which(insurance.nhis==1),])) # create data for table
-  rct.nrt.tab$group <- NA
-  rct.nrt.tab$group[rct.nrt.tab$study=="OHIE" & rct.nrt.tab$treatment==0] <- 1
-  rct.nrt.tab$group[rct.nrt.tab$study=="OHIE" & rct.nrt.tab$treatment==1] <- 2
-  rct.nrt.tab$group[rct.nrt.tab$study=="NHIS" & rct.nrt.tab$treatment==1] <- 3
-    
-  tableNominal(vars = rct.nrt.tab, 
-               group = rct.nrt.tab$group, 
-         #      nams=c(cov.names[-1],"Any ER visit","Any primary care visit"),
-               vertical=FALSE,
-               prec = 3,cumsum=FALSE,lab = "rct-nrt-compare",
-               cap="Pretreatment covariates and responses for the OHIE and for NHIS respondents who received Medicaid.") # RCT vs. NRT compliers
-}
-
 
